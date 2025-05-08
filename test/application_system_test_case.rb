b97ddffed37dae881570c98adc40ad08720fed17
FalconRage <rwmathot@me.com>
2025-05-08 23:25:49 +0200

    indo the changes

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,15 +3,3 @@ require "test_helper"
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
 end
-
-
-ef
-eswf
-screen_sizefd
-f
-screen_sizedfs
-v
-sv
-screen_sizevsd
-v
-screen_sizedfsv
